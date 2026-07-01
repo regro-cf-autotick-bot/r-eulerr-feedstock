f780ed553cb93c31371ff7fca69fdd40a28ac131
Mervin Fansler <mervin.fansler@bric.ku.dk>
2026-07-01 13:25:02 +0200

    bundle rust licenses
    Add script to bundle third party licenses from Rust crates

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,3 +2,8 @@
 export DISABLE_AUTOBREW=1
 # shellcheck disable=SC2086
 ${R} CMD INSTALL --build . ${R_ARGS}
+
+# Bundle third party licenses from statically linked crates
+pushd src/rust
+cargo-bundle-licenses --format yaml --output "${SRC_DIR}/THIRDPARTY.yml"
+popd
